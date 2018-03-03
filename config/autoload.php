cb88738376e7720d9b118a1cf8a3186814cd54d5
Datros Sumendong <dsumendong@gmail.com>
2018-03-03 16:21:23 +0800

    Memulai Project

---
+++ config/autoload.php
@@ -0,0 +1,36 @@
+<?php
+
+  /**
+   *  Spl_Autoload_Register
+   *  Load semua class yang ingin kamu gunakan di sini;
+   */
+  spl_autoload_register(function($class)
+  {
+
+    /**
+     * Ini adalah sources tempat file class berada
+     */
+    $sources=array(
+                   "database/$class.php",
+                   "mvc/$class.php",
+                   "loopers/time/$class.php",
+                   "loopers/token/$class.php",
+                   "loopers/redirect/$class.php",
+                   "loopers/validation/$class.php",
+                   "loopers/dibug/$class.php"
+                  );
+
+    /**
+     * Memanggil semua file class
+     */
+    foreach($sources as $source)
+    {
+      if(file_exists($source))
+      {
+       require_once $source;
+      }
+    }
+
+  });
+
+?>
--- index.php
+++ index.php
@@ -1,5 +1,19 @@
 <?php
 
-  echo "Hello World!!!";
+  /**
+   * @Spl_autoload_register
+   */
+  require_once 'config/autoload.php';
+
+  /**
+   * @START SESSION
+   */
+  session_start();
+
+  /**
+   * @Memulai routing controllers
+   */
+  $route=new route();
+  $route->init(); //Memanggil init load Controller dan Methode
 
 ?>
